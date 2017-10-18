81db2552381effe7c3bcd3e39b8b8c06a83e8f00
Stuart Leal <stuart.leal23@gmail.com>
2017-10-18 08:24:46 -0600

    Vesion final del arbol con delete, insertcomplete y printtree

--- Lab8/arbol.h
+++ Lab8/arbol.h
@@ -3,6 +3,8 @@
 #include<string>
 #include<vector>
 #include "cola2.h"
+//#include<stack>
+#include<queue>
 
 #ifndef CLASEARBOL
 #define CLASEARBOL
@@ -144,79 +146,285 @@ class C_BinTree {
 
   void delete_tree(int dato){
     // Funcion dummy para iniciar recursion
-    delete(dato, this->raiz);
+    //    cout << "Aqui" << endl;
+    delete_tree(dato, this->raiz);
   }
   
-  void delete_tree(int dato, S_node * nodo){
-    if(nodo->dato==dato){
-      // En el nodo actual, el dato es el que se necesita
-      // Primero hay que revisar si está o no
-      
-      // Si el nodo es hoja lo elimina
-      
-      // Si el nodo tiene un hijo entonces
-      // el hijo ocupa ese espacio
-      
-      // Tiene dos hijos Se busca el sucesor
-      // en orden y  este pasa a sustituir
-      // el nodo que se elimina
+  bool delete_tree(int dato, S_node * nodo){
+    //    cout << "Entre" << endl;
+    // Si en el siguiente nodo esta el dato:
+    if(!nodo){
+      // Si en lo que estoy no es un nodo entonces devolverme
+      return false;
+    }
+
+
+    //    cout << nodo->left->dato << endl;
+    // Si el nodo a la derecha tiene el valor deseado hay que evaluar tres cosas    
+    if((nodo->right && nodo->right->dato == dato) || (nodo->left && nodo->left->dato == dato)){
+      S_node * hijo;
+      //      S_node * temp;
+      if(nodo->left){
+	hijo = nodo->left;
+	
+      }
+      else{
+	hijo = nodo->right;
+      }
+
+      if(!hijo->left && !hijo->right){
+	// nodo hoja
+	if(nodo->left){
+	  nodo->left = NULL;
+	}
+	else{
+	  nodo->right = NULL;
+	}
+	//hijo = NULL;
+	return true;
+      }
+
+      if((!hijo->left && hijo->right) || (!hijo->right && hijo->left)){
+	// nodo solo tiene un hijo
+	//	cout << "Caso de solo un hijo" << endl;
+	if(hijo->left){
+	  // El unico hijo es el izquierdo, entonces el izquierdo sube
+	  if(nodo->left){
+	    nodo->left = hijo->left;
+	  }
+	  else{
+	    nodo->right = hijo->left;
+	  }//else
+	}//if hijo left
+	else{
+	  // hijo->right
+	  if(nodo->left){
+	    nodo->left = hijo->right;
+	  }
+	  else{
+	    nodo->right = hijo->right;
+	  }
+	}//else
+	return true;
+      }
+
+      if(hijo->left && hijo->right){
+	S_node * temp;
+	S_node * aux = new S_node();
+	temp = hijo->left;
+	while(!temp){
+	  // Mientras temp no sea null
+	  temp = temp->left;
+	}
+	int nuevo_dato = temp->left->dato;
+	aux->dato = nuevo_dato;
+	delete temp->left;
+	temp->left = NULL;
+	
+	
+	if(nodo->left){
+	  aux->left = nodo->left->left;
+	  aux->right = nodo->left->right;
+	  nodo->left = aux;
+	}// if nodo left
+	else{
+	  aux->left = nodo->right->left;
+	  aux->right = nodo->right->right;
+	  nodo->right = aux;
+	} // else nodo left
+	return true;
+      }
       
 
+      delete hijo;
+      return true;
     }
-    else{
-      // El nodo actual no contiene el dato que necesito
-      delete(dato, nodo->left);
-      delete(dato, nodo->right);
+    if(delete_tree(dato, nodo->right)){
+      return true;
     }
-    
+    if(delete_tree(dato, nodo->left)){
+      return true;
+    }
+    return false;
   }// delete
 
+  struct Trunk{
+    Trunk *prev;
+    string str;
+    
+    Trunk(Trunk *prev, string str)
+    {
+      this->prev = prev;
+      this->str = str;
+    }
+  };
+
+  // Helper function to print branches of the binary tree                                                                                                                                                                                                                         
+  void showTrunks(Trunk *p){
+    if (p == nullptr)
+      return;
+    
+    showTrunks(p->prev);
+    
+    cout << p->str;
+  }
+
+  void printTree(void){
+    printTree(this->raiz, nullptr, false);
+  }
+
+  void printTree(S_node *root, Trunk *prev, bool isLeft)
+  {
+    if (root == nullptr)
+      return;
+    
+    string prev_str = "     ";
+    Trunk *trunk = new Trunk(prev, prev_str);
+    
+    printTree(root->left, trunk, true);
+    
+    if (!prev)
+      trunk->str = "---";
+    else if (isLeft)
+      {
+	trunk->str = ".---";
+	prev_str = "   |";
+      }
+    else
+      {
+	trunk->str = "`---";
+	prev->str = prev_str;
+      }
+    
+    showTrunks(trunk);
+    cout << root->dato << endl;
+    
+    if (prev)
+      prev->str = prev_str;
+    trunk->str = "   |";
+    
+    printTree(root->right, trunk, false);
+  }
+  
   void insertComplete(int dato){
     // Insertar para mantener un
     // arbol completo
+    // Si se puede insertar de tal forma
+    // Que no se aumente la altura
+    // Entonces se inserta ahi
+    S_node * aux = new S_node();
+    aux->dato = dato;
+    aux->left = NULL;
+    aux->right = NULL;
+
+    insertComplete(aux, this->raiz);
+
   }// insertComplete
 
-  void printTree(void){
+
+  bool insertComplete(S_node * nodo_nuevo, S_node * nodo){
+    // Se prueba si en el nodo actual, hay
+    // un espacio en nodo->left o nodo->right
+    // si no se entra en recursion
+    
+
+    queue<S_node*> cola;
+
+    //    cola.agregar(nodo);
+    cola.push(nodo);
+    while(cola.empty() == false){
+      //nodo = cola.quitar();
+      nodo = cola.front();
+      cola.pop();
+      if(nodo==NULL){
+	//cout << "NULL" << endl;
+	continue;
+      }
+      //cout << nodo->dato << endl;
+      if(!nodo->left){
+	nodo->left = nodo_nuevo;
+	return true;
+      }
+
+      if(!nodo->right){
+	nodo->right = nodo_nuevo;
+	return true;
+      }
+
+      if(nodo->left){
+	cola.push(nodo->left);
+	//cola.agregar(nodo->left);
+      }//if
+      else{
+	nodo->left = nodo_nuevo;
+	return true;
+      }
+      if(nodo->right){
+	cola.push(nodo->right);
+
+      }//if
+      else{
+	nodo->right = nodo_nuevo;
+	return true;
+	//cola.agregar(NULL);
+      }
+      //delete nodo;
+    }
+  }
+
+  void printTree_test(void){
     // imprimir con forma de arbol
     S_node * nodo = this->raiz;
     string dash_izq = "/";
     string dash_der = "\\";
     string enter = "\n";
     string espacio = "  ";
-    C_cola2<S_node*> cola;
-    cout << "arbol a mano" << endl;
-    cout << nodo->dato << endl;
-    cout << nodo->left->dato << endl;
-    cout << nodo->right->dato << endl;
-    cout << nodo->left->left->dato << endl;
-    cout << nodo->left->left->left->dato << endl;
+    //int piso = height() - 1;
+    
+    
+    queue<S_node*> cola;
 
-    cout << "" << endl;
-    cola.agregar(nodo);
-    while(cola.is_empty() == false){
-      nodo = cola.quitar();
+    //    cola.agregar(nodo);
+    cola.push(nodo);
+    while(cola.empty() == false){
+      //nodo = cola.quitar();
+      nodo = cola.front();
+      cola.pop();
       if(nodo==NULL){
 	cout << "NULL" << endl;
 	continue;
       }
       cout << nodo->dato << endl;
       if(nodo->left){
-	cola.agregar(nodo->left);
+	cola.push(nodo->left);
+	//cola.agregar(nodo->left);
       }//if
       else{
-	cola.agregar(NULL);
+	cola.push(NULL);
+	//cola.agregar(NULL);
       }
       if(nodo->right){
-	cola.agregar(nodo->right);
+	cola.push(nodo->right);
+	//cola.agregar(nodo->right);
       }//if
       else{
-	cola.agregar(NULL);
+	cola.push(NULL);
+	//cola.agregar(NULL);
       }
       delete nodo;
     }
     cout << "" << endl;
     
   }// printTree
+
+
+  void printBranches(int loop, int whitespaces){
+    for(int i=0; i<loop; i++){
+      
+    }
+
+  }// printBranches
+  
 };
 
 
