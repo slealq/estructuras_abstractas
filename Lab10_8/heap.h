c58762881c3bb9fdd1edce8146cbe836f528143a
Stuart Leal Quesada <stuart.leal23@gmail.com>
2017-10-20 18:18:19 -0600

    Created directory for next Lab

---
+++ Lab10_8/heap.h
@@ -0,0 +1,22 @@
+// heap.h
+#include<iostream>
+#include<vector>
+
+#ifndef CLASEARBOL
+#define CLASEARBOL
+
+using namespace std;
+
+// vector
+
+class C_Heap {
+ protected:
+  vector<int> heap;
+  int htype;
+ private:
+  //variables o metodos privados
+
+  
+};
+
+#endif
