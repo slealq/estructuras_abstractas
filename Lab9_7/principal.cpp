d92c315bca691567e2c3c9dfc630a027fb71e339
Stuart Leal <stuart.leal23@gmail.com>
2017-10-25 04:15:30 -0600

    Prueba completa que contiene los tres casos de delete

--- Lab9_7/principal.cpp
+++ Lab9_7/principal.cpp
@@ -34,7 +34,37 @@ void prueba3(void){
   cout << "Ahora borro el nodo 6: 1 hijo" << endl;
   arbol.deleteAVL(6);
   
-  
+  arbol.printTree();
+  cout << endl;
+
+  cout << "Vamos a insertar varios datos mas: 6, 40, 4, 7, 2" << endl;
+  cout << endl;
+  arbol.insertAVL(6);
+  arbol.insertAVL(40);
+  arbol.insertAVL(4);
+  arbol.insertAVL(7);
+  arbol.insertAVL(2);
+
+  arbol.printTree();
+  cout << endl;
+
+  cout << "Ahora vamos a borrar un nodo con dos hijos: 19" << endl;
+  arbol.deleteAVL(19);
+
+  cout << endl;
+  cout << "Resultado: " << endl;
+
+  cout << endl;
+  arbol.printTree();
+  cout << endl;
+
+  cout << "Borrar otro nodo con dos hijos: 4" << endl;
+  arbol.deleteAVL(4);
+
+  cout << endl;
+  cout << "Resultado: " << endl;
+
+  cout << endl;
   arbol.printTree();
   
   cout << endl;
