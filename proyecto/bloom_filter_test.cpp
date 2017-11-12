a426a2e8090c71f0af833db10d170bcf2d080f70
Stuart Leal <stuart.leal23@gmail.com>
2017-11-12 01:32:45 -0600

    Simple test for assertion.

--- proyecto/bloom_filter_test.cpp
+++ proyecto/bloom_filter_test.cpp
@@ -4,5 +4,8 @@
 int main(void) {
   std::cout << "Testing and addition to the bloom filter..." << std::endl;
   bloom_filter filter(10, 2);
-  filter.add(
+  uint8_t * test = new uint8_t(2);
+  filter.add(test, 3);
+  std::cout << filter.possibly_contained(new uint8_t(2), 3) << std::endl;
+  std::cout << filter.possibly_contained(new uint8_t(3), 3) << std::endl;
 }
