519763282eda1c7c7939503b2b9946133f46bd87
Stuart Leal <stuart.leal23@gmail.com>
2017-11-12 01:32:23 -0600

    Changed the int structures for double hashing tecnique

--- proyecto/bloom_filter.cpp
+++ proyecto/bloom_filter.cpp
@@ -2,14 +2,14 @@
 #include "bloom_filter.h"
 #include "smhasher/src/MurmurHash3.h"
 
-bloom_filter::bloom_filter(int m_bits, int k_hashes){
+bloom_filter::bloom_filter(uint64_t m_bits, uint8_t k_hashes){
   filter_bits_.resize(m_bits, false);
-  k_hashes_ = k_hashes; // fancy way to initialize
+  k_hashes_ = k_hashes;
   m_bits_ = m_bits;
 }//bloom_filter constructor
 
 //helper 164 bit hashing function with murmurhash3
-std::array<uint64_t, 2> hash(const int *data, std::size_t length){
+std::array<uint64_t, 2> hash(const uint8_t * data, std::size_t length){
   // array of unsigned ints of 64 bits is the output
   std::array<uint64_t, 2> hash_value;
   MurmurHash3_x64_128(data, length, 0, hash_value.data());
@@ -19,7 +19,10 @@ std::array<uint64_t, 2> hash(const int *data, std::size_t length){
 }// hash
 
 //helper double hashing function for 0 to k different hashes
-uint64_t ith_hash(int i, int hash_a, int hash_b, int m_bits_){
+uint64_t ith_hash(uint8_t i,
+		  uint64_t hash_a,
+		  uint64_t hash_b,
+		  uint64_t m_bits_){
   // --> formula: hash_i(x, m) = { hash_a(x) + i*hash_b(x)  } % m
   // --> i is the iest from 0 to k
   // --> m is the maximum result the hash should give
@@ -29,7 +32,7 @@ uint64_t ith_hash(int i, int hash_a, int hash_b, int m_bits_){
 
 } // double hashing
 
-void bloom_filter::add(int * data, std::size_t length){
+void bloom_filter::add(uint8_t * data, std::size_t length){
   // auto for automatic type 
   auto hash_values = hash(data, length);
 
@@ -42,3 +45,24 @@ void bloom_filter::add(int * data, std::size_t length){
   }// for
   
 }//add function
+
+bool bloom_filter::possibly_contained(uint8_t * data,
+				      std::size_t length){
+  // auto for automatic type
+  auto hash_values = hash(data, length);
+
+  for( int i = 0; i < k_hashes_; i++){
+    // check that all bits from the hashes are true
+    if(!filter_bits_[ith_hash(i,
+			     hash_values[0],
+			     hash_values[1],
+			      m_bits_)]) {
+	 return false;
+	 
+      }//if
+
+       // if all are high return true
+       return true; // might be in the filter
+  }//for
+  
+}// possibly contained function
