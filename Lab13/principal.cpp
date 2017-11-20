a29886dbdeb994670ce34ce84c7dbd6d9d6b3bff
Stuart Leal <stuart.leal23@gmail.com>
2017-11-20 17:45:02 -0600

    Inicio del ultimo laboratorio.

---
+++ Lab13/principal.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <functional>
+#include <utility>
+
+int main(void) {
+  std::vector<int> v {10, 5, 100, 50, 20, 15};
+  priority_queue<int, vector<int>, greater<int>> pqMIN;
+
+  for(int i = 0; i
+  
+} //main
